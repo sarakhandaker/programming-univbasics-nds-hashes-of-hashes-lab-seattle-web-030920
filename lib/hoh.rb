4f56a74d304adadcd42d862267f2947477da958a
Sara Khandaker <kttsara@hotmail.com>
2020-02-12 04:33:34 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -44,6 +44,6 @@ def naming_system
   # HoH all the way to Species!
 hoh=  PORTION_1
 hoh[:sub_category][:sub_category][:sub_category]= PORTION_2
-hoh[:sub_category][:sub_category][:sub_category][sub_category]=
+hoh[:sub_category][:sub_category][:sub_category][:sub_category]=PORTION_3
 hoh
 end
